de9a0ee3374e8d3c9b2097519dc47cb3d72a0d10
Parvathy Lakshmibai <Lakshmibai-P@ulster.ac.uk>
2025-10-06 15:23:54 +0100

    updated

--- week3/Program.cs
+++ week3/Program.cs
@@ -7,9 +7,10 @@ class Program
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates","1234567");
-        Book book = new Book("Ultimate C# ", "Microsoft", "2233445");
+        Book book1 = new Book("Ultimate C# ", "Microsoft", "2233445");
 
         book.DisplayInfo();
+        book1.DisplayInfo();
     }
 }
 
