909e507479817e5f157a6580b90c88de4031dce4
Parvathy Lakshmibai <Lakshmibai-P@ulster.ac.uk>
2025-10-06 15:43:54 +0100

    Updated code with a new Member class

---
+++ week3/Member.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+    
+
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name
+        {
+            get { return name; }  // get method
+            set { name = value; } // set method
+        }
+        public string Address
+        {
+            get { return address; }  // get method
+            set { address = value; } // set method
+        }
+        public int Phone
+        {
+            get { return phone; }  // get method
+            set { phone = value; } // set method
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member Id: {memberId}");
+            Console.WriteLine($"Member Name: {name}");
+            Console.WriteLine($"Member Address: {address}");
+            Console.WriteLine($"Member Phone: {phone}");
+        }
+
+    }
+}
+namespace Library
+{
+    class Book
+    {
+        private string title; // private variable
+        private string author;// private variable
+        private string isbn; // private variable
+
+        // Title property allows access
+        // to the title private variable
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
+        }
+
+        // Method to display information about a book
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+       
+    }
+}
