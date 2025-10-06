3fd0dd1103dccef27870b03f48b26c11a2c6f2ea
Parvathy Lakshmibai <Lakshmibai-P@ulster.ac.uk>
2025-10-06 15:22:22 +0100

    Updated code with variables and properties

--- week3/Book.cs
+++ week3/Book.cs
@@ -8,15 +8,31 @@ namespace week3
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
 
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN;
         }
 
         public void DisplayInfo()
--- week3/Program.cs
+++ week3/Program.cs
@@ -7,6 +7,7 @@ class Program
         // Create a new instance (object) of the Book class
         // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates","1234567");
+        Book book = new Book("Ultimate C# ", "Microsoft", "2233445");
 
         book.DisplayInfo();
     }
